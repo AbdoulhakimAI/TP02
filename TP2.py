a97fb299179c7b44734b758c4bb2e107df24d117
lexi255 <alexakassar@hotmail.com>
2024-09-27 14:33:36 -0400

    correction partie 1 2 3

--- TP2.py
+++ TP2.py
@@ -16,7 +16,9 @@ csvfile= open('collection_bibliotheque.csv', newline='')
 collection_initiale = csv.DictReader(csvfile)
 bibliotheque ={}
 for row in collection_initiale:
-    bibliotheque.update({row['cote_rangement']: row['titre'] + " , " + row['auteur']+ " , " + row['date_publication']})
+    bibliotheque[row['cote_rangement']] = {"titre":row['titre'], "auteur" : row['auteur'], "date_publication" : row['date_publication']}
+
+    #bibliotheque.update({row['cote_rangement']: row['titre'] + " , " + row['auteur']+ " , " + row['date_publication']})
 
 print(f' \n Bibliotheque initiale: {bibliotheque} \n')
 
@@ -27,59 +29,58 @@ print(f' \n Bibliotheque initiale: {bibliotheque} \n')
 
 csvfile2= open('nouvelle_collection.csv', newline='')
 nouvelle_collection = csv.DictReader(csvfile2)
-bibliotheque2 = {}
+
 
 for new_row in nouvelle_collection:
-    bibliotheque2.update({new_row['cote_rangement']: new_row['titre'] + " , " + new_row['auteur'] + " , " + new_row['date_publication']})
+    cote = new_row['cote_rangement']
+    titre2 = new_row['titre'] 
+    auteur2 = new_row['auteur'] 
+    date_publication2 = new_row['date_publication']
 
-
-keys2 = list(bibliotheque2.keys())
-keys1 =list(bibliotheque.keys())
-
-for new_key in keys2:
-
-    newlist=[]
-    newlist.extend(bibliotheque2[new_key].split(","))
-
-    if new_key in keys1:
-        print(f'Le livre {new_key} ---- {newlist[0]} par {newlist[1]} ---- est déjà présent dans la bibliothèque')
-    else: 
-        bibliotheque[new_key] = bibliotheque2[new_key]  
-        print(f'Le livre {new_key} ---- {newlist[0]} par {newlist[1]} ---- a été ajouté avec succès')
-
+    if cote in bibliotheque.keys():
+        print(f'Le livre {cote} ---- {titre2} par {auteur2} ---- est déjà présent dans la bibliothèque')
+    else:
+        bibliotheque[cote] = {"titre": titre2 , "auteur" : auteur2 , "date_publication" : date_publication2}
+        print(f'Le livre {cote} ---- {titre2} par {auteur2} ---- a été ajouté avec succès')
+    
 ########################################################################################################## 
 # PARTIE 3 : Modification de la cote de rangement d'une sélection de livres
 ########################################################################################################## 
-#1ere etape : acceder aux livres de william shaksperare
-#2eme etape modifier le key
 
-for key in list(bibliotheque.keys()):
-    list_of_information = []
-    list_of_information.extend(bibliotheque[key].split(","))
-   
-    if "William Shakespeare" in list_of_information[1]:
-        bibliotheque[key.replace("S" , "WS")]= bibliotheque[key]
-        bibliotheque.pop(key, None)
+copie_bibliotheque = bibliotheque.copy()
+
+for key, value in copie_bibliotheque.items():
+    if value['auteur'] == "William Shakespeare" and key[0] == "S":
+        new_key = key.replace("S" , "WS")
+        bibliotheque[new_key] = bibliotheque.pop(key)
         
+
 print(f' \n Bibliotheque avec modifications de cote : {bibliotheque} \n')
+
 
 ########################################################################################################## 
 # PARTIE 4 : Emprunts et retours de livres
 ########################################################################################################## 
 
-# TODO : Écrire votre code ici
+csvfile3 = open('emprunts.csv', newline='')
+liste_emprunt = csv.DictReader(csvfile3)
 
+emprunts= {}
 
+    cote_rangement = row1['cote_rangement']
+    date_emprunt = row1['date_emprunt']
 
+    for key_emprunt in bibliotheque.keys():
+        if key_emprunt in cote_rangement:
+            bibliotheque[key_emprunt]['emprunts'] = "emprunté"
+            bibliotheque[key_emprunt]['date_emprunt'] = date_emprunt
+        else:
+            bibliotheque[key_emprunt]['emprunts'] = "disponible"
 
-
-
+print(f' \n Bibliotheque avec ajout des emprunts : {bibliotheque} \n')
 
 ########################################################################################################## 
 # PARTIE 5 : Livres en retard 
 ########################################################################################################## 
 
 # TODO : Écrire votre code ici
-
-
-
