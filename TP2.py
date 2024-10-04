780553293306a231ed5ba40910260eff8556c63a
lexi255 <alexakassar@hotmail.com>
2024-10-04 13:51:46 -0400

    final version

--- TP2.py
+++ TP2.py
@@ -2,8 +2,9 @@
 TP2 : Système de gestion de livres pour une bibliothèque
 
 Groupe de laboratoire : 01
-Numéro d'équipe :  Y25
-Noms et matricules : Alexa Kassar(2350528), Nom2 (Matricule2)
+Numéro d'équipe :  L25
+Noms et matricules : Alexa Kassar(2350528), Abdoulhakim Abdillahi Ibrahim (2221997)
+
 """
 
 ########################################################################################################## 
@@ -11,27 +12,33 @@ Noms et matricules : Alexa Kassar(2350528), Nom2 (Matricule2)
 ########################################################################################################## 
 
 import csv
+#parcourir collection_bibliotheque.csv et le lire comme un dictionnaire
 
 csvfile= open('collection_bibliotheque.csv', newline='')
 collection_initiale = csv.DictReader(csvfile)
 bibliotheque ={}
 
+#créer la bibliotheque
 for row in collection_initiale:
 	bibliotheque[row['cote_rangement']] = {"titre":row['titre'], "auteur" : row['auteur'], "date_publication" : row['date_publication']}
 
 print(f' \n Bibliotheque initiale: {bibliotheque} \n')
 
+csvfile.close()
 
 ########################################################################################################## 
 # PARTIE 2 : Ajout d'une nouvelle collection à la bibliothèque
 ########################################################################################################## 
 
+#parcourir la nouvelle collection, DictReader permet de la lire comme un dictionnaire
+
 csvfile2= open('nouvelle_collection.csv', newline='')
 nouvelle_collection = csv.DictReader(csvfile2)
 
+#accéder aux cotes, titres et auteurs de la nouvelle collection
 
 for new_row in nouvelle_collection:
-	cote = new_row['cote_rangement']
+	cote = new_row['cote_rangement'] 
 	titre2 = new_row['titre'] 
 	auteur2 = new_row['auteur'] 
 	date_publication2 = new_row['date_publication']
@@ -39,10 +46,13 @@ for new_row in nouvelle_collection:
 	if cote in bibliotheque.keys():
 		print(f'Le livre {cote} ---- {titre2} par {auteur2} ---- est déjà présent dans la bibliothèque')
 
+# ajouter les livres qui n'ont pas de doublure dans la bibliothèque
+
 	else:
 		bibliotheque[cote] = {"titre": titre2 , "auteur" : auteur2 , "date_publication" : date_publication2}
 		print(f'Le livre {cote} ---- {titre2} par {auteur2} ---- a été ajouté avec succès')
 
+csvfile2.close()
 
 ########################################################################################################## 
 # PARTIE 3 : Modification de la cote de rangement d'une sélection de livres
@@ -52,8 +62,8 @@ copie_bibliotheque = bibliotheque.copy()
 
 for key, value in copie_bibliotheque.items():
 	if value['auteur'] == "William Shakespeare" and key[0] == "S":
-		new_key = key.replace("S" , "WS")
-		bibliotheque[new_key] = bibliotheque.pop(key)
+		new_key = key.replace("S" , "WS") #remplacer la cote des livres
+		bibliotheque[new_key] = bibliotheque.pop(key) #supprimer l'ancienne key
 		
 print(f' \n Bibliotheque avec modifications de cote : {bibliotheque} \n')
 
@@ -62,23 +72,26 @@ print(f' \n Bibliotheque avec modifications de cote : {bibliotheque} \n')
 # PARTIE 4 : Emprunts et retours de livres
 ########################################################################################################## 
 
+# parcourir emprunt.csv comme un dictionnaire 
+
 csvfile3 = open('emprunts.csv', newline='')
 liste = csv.DictReader(csvfile3)
 liste_emprunt = {}
-for column in liste:
+
+for column in liste: #création d'une liste de livres empruntés
 	liste_emprunt[column['cote_rangement']] = column['date_emprunt']
 
-print(len(liste_emprunt))
-
-for cote in bibliotheque.keys():
+for cote in bibliotheque.keys(): #vérifier lesquels des livres de la biblio sont empruntés
+#ajout de nouvelles clés
 	if cote in liste_emprunt.keys():
-		bibliotheque[cote]['emprunts'] = "emprunté"
+		bibliotheque[cote]['emprunts'] = "emprunté" 
 		bibliotheque[cote]['date_emprunt'] = liste_emprunt[cote]
 	else:
-		bibliotheque[cote]['emprunts'] = "Disponible"
+		bibliotheque[cote]['emprunts'] = "disponible"
 		   
 print(f' \n Bibliotheque avec ajout des emprunts : {bibliotheque} \n')
 
+csvfile3.close()
 
 ########################################################################################################## 
 # PARTIE 5 : Livres en retard 
@@ -87,25 +100,45 @@ print(f' \n Bibliotheque avec ajout des emprunts : {bibliotheque} \n')
 import datetime
 from datetime import datetime
 
-now = datetime.now()
+now = datetime.now() #date actuelle
 livres_en_retard = []
+livres_perdus = []
 
 for key, date in liste_emprunt.items():
+	for key1 in bibliotheque.keys():
+		if key1 == key:
   
-	date_emprunt = datetime.strptime(date, '%Y-%m-%d')
-	number_of_days = (now - date_emprunt).days
+			date_emprunt = datetime.strptime(date, '%Y-%m-%d') #convertir en date
+			number_of_days = (now - date_emprunt).days  #retard en jours
 
-	if number_of_days > 365:
-		bibliotheque[key]["livres_perdus"]= "livre est perdu"
-	
-	elif 30 < number_of_days < 365 :
-		jours_de_retard = number_of_days - 30
+			if number_of_days >= 365: #trouver les livres perdus
+				bibliotheque[key1]["livres_perdus"]= "livre est perdu"
+				bibliotheque[key1]["frais_retard"]= "100 $ "
+				livres_perdus.append(bibliotheque[key1]["titre"])
 
-		#min compare le frais calculé à la valeur 100 et choisit la plus petite valeur càd : frais max = 100
-		frais = min(jours_de_retard*2, 100)
+			else: 
+				bibliotheque[key1]["livres_perdus"]= " ce livre n'est pas perdu"
+				
 
-		bibliotheque[key]["frais_retard"]= frais
-		livres_en_retard.append(f' Livre: {bibliotheque[key]["titre"]} , frais de retard: {bibliotheque[key]["frais_retard"]} $')
-		
+			if number_of_days >30: 
+				jours_de_retard = number_of_days - 30 #frais appliquables sur les jours après le delai de retour
+
+			#min compare le frais calculé à la valeur 100 et choisit la plus petite valeur càd : frais max = 100
+				frais = min(jours_de_retard*2, 100)
+				bibliotheque[key1]["frais_retard"]= str(frais) + " $" 
+
+				#création d'une liste de livres en retard
+				livres_en_retard.append(f' Livre: {bibliotheque[key1]["titre"]} , frais de retard: {bibliotheque[key1]["frais_retard"]} $')
+
+			else: 
+				bibliotheque[key1]["frais_retard"]= "0 $"
+
+			
+		else: 
+			bibliotheque[key1]["livres_perdus"]= "livre n'est pas perdu"
+			bibliotheque[key1]["frais_retard"]= "0 $"
+
+print("la liste des livres perdus est:",  (livres_perdus))
 print("La liste des livres en retard est:", (livres_en_retard))
 print(f' \n Bibliotheque avec ajout des retards et frais : {bibliotheque} \n')
+
